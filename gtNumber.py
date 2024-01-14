2c7db020c982ff8da1bf661fc05c7f89ed9e0b5d
Ali Volkan <avolkan99@gmail.com>
2024-01-14 18:02:29 +0000

    player guess if statment with excepttion

--- gtNumber.py
+++ gtNumber.py
@@ -1,8 +1,22 @@
-# main.py
+# Import Random Library
 import random
 
-# define range and max_attempts
+# Guessing Range and Max number of attemps 
 lower_bound = 1
 upper_bound = 1000
 max_attempts = 10
 
+# Generate random number within Lower and upp range 
+secret_num = random.randint(lower_bound, upper_bound)
+
+# Players guess 
+def player_guess():
+    while True:
+        try:
+            guess = int(input(f"Guess a number between {lower_bound} and {upper_bound}: "))
+            if lower_bound <= guess <= upper_bound:
+                return guess
+            else:
+                print("Oh No!! This number is out of range !!. Please enter a number within the range.")
+        except ValueError:
+            print("Oh !! That's Not a Number !! . Please enter a number in range.")
