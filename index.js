e479c57215071dbd6c291a3a1229e0f4a8ad1711
Ali Volkan <avolkan99@gmail.com>
2024-01-16 19:31:50 +0000

    "port"

--- index.js
+++ index.js
@@ -6,8 +6,7 @@
 const options = {};
 
 // options.ip = '127.0.0.1';
-//options.port = parseInt(process.env.PORT || 8000);
-var port = process.env.PORT || 8080
+options.port = parseInt(process.env.PORT );
 // options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
